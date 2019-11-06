63b0a72998d028bf35dc00fbd3f6fd045a10c75f
elleon003 <ngrandison@ymail.com>
2019-11-05 20:52:05 -0500

    Add Eraser functionality

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const container = document.querySelector('.container');
 const clear = document.querySelector('.clear');
+const buildGrid = document.querySelector('.buildGrid');
+const eraser = document.querySelector('.eraser');
+const color = document.querySelector('.color');
 
 // Grid creation
 const createGrid = (cells) => {
-    container.style.gridTemplateColumns = `repeat(${cells}, 25px)`;
+    container.style.gridTemplateColumns = `repeat(${cells}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${cells}, 1fr)`;
     for (let i = 0; i < cells; i++) {
         for (let j = 0; j < cells; j++) {
             const div = document.createElement('div');
             div.classList.add('item');
-            div.style.width = '25px';
-            div.style.height = '25px';
-            div.style.border = 'solid black 1px'
             container.appendChild(div);
         }
     }
-    // colorizing the grid
+    // colorize functionality
     const item = document.querySelectorAll('.item');
     item.forEach((selection) => {
         selection.addEventListener('mouseenter', (e) => {
@@ -26,8 +27,29 @@ const createGrid = (cells) => {
         item.forEach((selection) => {
             selection.classList.remove('purple');
         })
-
+    });
+    // eraser
+    eraser.addEventListener('click', () => {
+        item.forEach((selection) => {
+            selection.addEventListener('mouseenter', (e) => {
+                e.target.classList.remove('purple');
+            })
+        })
+    });
+    // redraw
+    color.addEventListener('click', () => {
+        item.forEach((selection) => {
+            selection.addEventListener('mouseenter', (e) => {
+                e.target.classList.add('purple');
+            })
+        })
     })
 };
 
-createGrid(30);
+// Resize draw
+buildGrid.addEventListener('click', () => {
+    let cells = Number(prompt('How thick do you want your pen? (Enter number between 4 - 100)'));
+    createGrid(cells);
+});
+
+createGrid(20);
